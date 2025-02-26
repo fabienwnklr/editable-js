037e997886cdae2e3dcce5e765da3e908bf84897
fabienwnklr <fabien.winkler@outlook.fr>
2025-02-26 22:42:03 +0100

    chore: setup config

--- packages/core/vite.config.ts
+++ packages/core/vite.config.ts
@@ -1,10 +1,24 @@
 import { defineConfig } from 'vite';
-import path from 'path';
+import { resolve } from 'node:path';
 
 export default defineConfig({
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, 'src'),
+      '@': resolve(__dirname, 'src'),
     },
   },
+  server: {
+    open: process.env.NODE_ENV !== 'test',
+  },
+  build: {
+    cssMinify: true,
+    minify: true,
+    lib: {
+      entry: resolve(__dirname, 'src/EditableJs.ts'),
+      name: 'EditableJs', // for iife and umd
+      fileName: 'EditableJs',
+      formats: ['iife', 'cjs', 'es', 'umd'],
+    },
+  },
+  plugins: [],
 });
